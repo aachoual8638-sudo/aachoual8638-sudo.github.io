b388d2939fb0c8680d58fdb5bad2263e7b1a3226
Asma ACHOUAL <a.achoual8638@uca.ac.ma>
2026-05-07 12:11:13 +0200

    init: cv project

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [react()],
+    base: '/aachoual8638-sudo.github.io/',
+})
